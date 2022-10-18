79bc47110884f56065a8b25307b46813926417c5
Boland Unfug <boland.unfug@gmail.com>
2022-10-18 11:07:18 +0100

    testing with classes

---
+++ classtesting.java
@@ -0,0 +1,34 @@
+// Would normally program this better but this is just key testing
+
+public class classtesting{
+
+    public String customerOrder(String order){
+        return order;
+    }
+
+    public String orderStock (String order){
+        return order;
+    }
+
+public static void main(String[] args) {
+    
+}
+
+}
+
+
+class Order{
+    int orderkey;
+}
+
+class Customer{
+    int customer;
+}
+
+class OrderList {
+     int orderlistkey;
+}
+
+class Stock{
+    int stockkey;
+}
